d3c6c63344688a67a2e96e87b486d4eebd910824
熊谷直樹 <j.a.r.n0405@gmail.com>
2016-02-09 14:24:32 +0900

    Initial Commit

---
+++ PagingMenuControllerTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PagingMenuControllerTest
+//
+//  Created by KumagaiNaoki on 2016/02/09.
+//  Copyright © 2016年 KumagaiNaoki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
